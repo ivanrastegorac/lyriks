627f6132cad5ece2c4461fd32d8cb90c189629c3
Ivan Raštegorac <ivan.rastegorac@ag04.com>
2023-01-08 21:13:24 +0100

    Fixed styles in SongCard and made PlayPause component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
 import PlayPause from './PlayPause';
 
 const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
-  const handlePauseClick = () => {};
+  const dispatch = useDispatch();
 
-  const handlePlayClick = () => {};
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div
-          className={`absolute inste-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
             activeSong?.title === song.title
               ? 'flex bg-black bg-opacity-70'
               : 'hidden'
