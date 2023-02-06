3ee4fb1835e95cf0f27120daa35c8becefb820b6
Ivan Raštegorac <ivan.rastegorac@ag04.com>
2023-02-06 13:05:03 +0100

    Fixed SongDetails

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
@@ -10,18 +11,18 @@ import {
 
 const SongDetails = () => {
   const dispatch = useDispatch();
-  const { songid } = useParams();
+  const { songid, id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-  const { data: songData, isFetching: isFetchingSongDetails } =
-    useGetSongDetailsQuery({ songid });
   const {
     data,
-    isFetching: isFetchingRelatedSongs,
+    isFetching: isFetchinRelatedSongs,
     error,
   } = useGetSongRelatedQuery({ songid });
+  const { data: songData, isFetching: isFetchingSongDetails } =
+    useGetSongDetailsQuery({ songid });
 
-  if (isFetchingSongDetails && isFetchingRelatedSongs) {
+  if (isFetchingSongDetails && isFetchinRelatedSongs) {
     return <Loader title="Searching song details" />;
   }
 
@@ -39,12 +40,13 @@ const SongDetails = () => {
   return (
     <div className="flex flex-col">
       <DetailsHeader artistId={artistId} songData={songData} />
+
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
 
         <div className="mt-5">
           {songData?.sections[1].type === 'LYRICS' ? (
-            songData?.sections[1].text.map((line, i) => (
+            songData?.sections[1]?.text.map((line, i) => (
               <p
                 key={`lyrics-${line}-${i}`}
                 className="text-gray-400 text-base my-1"
@@ -54,7 +56,7 @@ const SongDetails = () => {
             ))
           ) : (
             <p className="text-gray-400 text-base my-1">
-              Sorry, no lyrics found!
+              Sorry, No lyrics found!
             </p>
           )}
         </div>
