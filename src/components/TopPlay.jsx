47d0e7a5b94b901df7a28882508797ccc30e70e2
Ivan Raštegorac <ivan.rastegorac@ag04.com>
2023-02-06 13:23:49 +0100

    Fixed TopPlay

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,11 +1,16 @@
-import { useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+/* eslint-disable import/no-unresolved */
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode } from 'swiper';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
 
 const TopChartCard = ({
   song,
@@ -15,7 +20,11 @@ const TopChartCard = ({
   handlePauseClick,
   handlePlayClick,
 }) => (
-  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+  <div
+    className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${
+      activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'
+    } py-2 p-4 rounded-lg cursor-pointer mb-2`}
+  >
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
@@ -24,10 +33,10 @@ const TopChartCard = ({
         alt={song?.title}
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
-        <Link to={`/songs/${song?.key}`}>
+        <Link to={`/songs/${song.key}`}>
           <p className="text-xl font-bold text-white">{song?.title}</p>
         </Link>
-        <Link to={`/songs/${song?.artists[0].adamid}`}>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
           <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
         </Link>
       </div>
@@ -75,6 +84,7 @@ const TopPlay = () => {
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
         </div>
+
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, i) => (
             <TopChartCard
@@ -89,6 +99,7 @@ const TopPlay = () => {
           ))}
         </div>
       </div>
+
       <div className="w-full flex flex-col mt-8">
         <div className="flex flex-row justify-between items-center">
           <h2 className="text-white font-bold text-2xl">Top Artists</h2>
@@ -96,6 +107,7 @@ const TopPlay = () => {
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
         </div>
+
         <Swiper
           slidesPerView="auto"
           spaceBetween={15}
@@ -105,16 +117,16 @@ const TopPlay = () => {
           modules={[FreeMode]}
           className="mt-4"
         >
-          {topPlays?.map((song, i) => (
+          {topPlays?.slice(0, 5).map((artist) => (
             <SwiperSlide
-              key={song?.key}
+              key={artist?.key}
               style={{ width: '25%', height: 'auto' }}
               className="shadow-lg rounded-full animate-slideright"
             >
-              <Link to={`/artists/${song?.artists[0].adamid}`}>
+              <Link to={`/artists/${artist?.artists[0].adamid}`}>
                 <img
-                  src={song?.images.background}
-                  alt="name"
+                  src={artist?.images?.background}
+                  alt="Name"
                   className="rounded-full w-full object-cover"
                 />
               </Link>
