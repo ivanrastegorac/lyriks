4a05710402a23f00567762dc1baada52aad24a91
Ivan Raštegorac <ivan.rastegorac@ag04.com>
2023-02-07 08:12:09 +0100

    Added ArtistDetails and new endpoints

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,12 +14,21 @@ export const shazamCoreApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `v1/charts/country?country_code=${countryCode}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
+    }),
     getSongDetails: builder.query({
-      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
-      getSongRelated: builder.query({
-        query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
-      }),
+      query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
     }),
   }),
 });
@@ -28,4 +37,5 @@ export const {
   useGetTopChartsQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
 } = shazamCoreApi;
